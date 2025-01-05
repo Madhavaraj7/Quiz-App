269c0bdf26075d8600d03cdeef1dc1fa0a9ddf7e
Madhavaraj R <raj266864@gmail.com>
2025-01-05 11:49:33 +0530

    updated

--- Frontend/src/services/quizService.js
+++ Frontend/src/services/quizService.js
@@ -1,5 +1,10 @@
+
+export const URL=import.meta.env.VITE_URL
+
+
+
 export const getQuizQuestions = async () => {
-    const response = await fetch('http://localhost:5000/api/quiz');
+    const response = await fetch(URL+'/api/quiz');
     if (!response.ok) {
       throw new Error('Failed to fetch quiz questions');
     }
